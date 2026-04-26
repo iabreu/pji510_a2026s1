845d96d3c37686565c358d501236748d6939de42
Ivan Abreu <iabreufilho@gmail.com>
2026-04-26 19:31:24 -0300

    Fix deploy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
+const webpack = require("webpack");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __dirname: JSON.stringify("/"),
+      }),
+    );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
